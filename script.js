58ee499700dd766a5d3f1daba843da4a8a8c7aaa
Elienay-Alves <elienay8486@gmail.com>
2022-01-04 13:49:12 -0300

    Capturei as tags no script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Primeiro vamos criar as variaveis para capturar as tags
+const listaTarefas = document.getElementById('lista-tarefas');
+const criarTarefas = document.getElementById('criar-tarefa');
+const textoTarefas = document.getElementById('texto-tarefa');
+
+//  Abaixo vamos criar as funções
