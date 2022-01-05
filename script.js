705fe1122224bb3f16897101e48807d1252ca1ee
Elienay-Alves <elienay8486@gmail.com>
2022-01-05 12:06:21 -0300

    fiz a funçao e xablau. Requisito 5 e 6 pronto. Dois coelhos numa cajadada so hahae

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // Primeiro vamos criar as variaveis para capturar as tags
 const listaTarefas = document.getElementById('lista-tarefas');
 const criarTarefas = document.getElementById('criar-tarefa');
-const textoTarefas = document.getElementById('texto-tarefa');
+const textoTarefa = document.getElementById('texto-tarefa');
 
 //  Abaixo vamos criar as funções
-function addTarefa(eventoDeOrigem) {
-  console.log(eventoDeOrigem.target);
-  console.log(eventoDeOrigem.type);
-}
+function addTarefa() {
+  const listaDeTarefas = textoTarefa.value;
+  const lista = document.createElement('li');
 
+  lista.innerText = listaDeTarefas;
+  listaTarefas.appendChild(lista);
+  // Aprendi a limpar o input no site https://www.geeksforgeeks.org/html-clearing-the-input-field/
+  textoTarefa.value = '';
+}
 // Criando o botão para adicionar as tarefas
 criarTarefas.addEventListener('click', addTarefa);
